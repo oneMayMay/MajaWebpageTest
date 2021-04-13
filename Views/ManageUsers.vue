abc58631ee62b685e09d9501bc6fc25907d15712
95markiewicz@gmail.com <95markiewicz@gmail.com>
2021-04-13 14:25:47 +0200

    fler sidor

---
+++ Views/ManageUsers.vue
@@ -0,0 +1,8 @@
+<template>
+<div> 
+    <h2>This is the home page </h2>
+        <a href="index.html">Click here to get to home page (html)</a>
+
+</template>
+
+    <a href="index.html">Click here to get to home page (html)</a>
