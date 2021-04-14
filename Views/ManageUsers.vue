e72dd32c50ea67fc4c45eec27aae618482982bf7
95markiewicz@gmail.com <95markiewicz@gmail.com>
2021-04-14 11:23:40 +0200

    Delat upp på sidor

--- Views/ManageUsers.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<div> 
-    <h2>This is the home page </h2>
-        <a href="index.html">Click here to get to home page (html)</a>
-
-</template>
-
-    <a href="index.html">Click here to get to home page (html)</a>
